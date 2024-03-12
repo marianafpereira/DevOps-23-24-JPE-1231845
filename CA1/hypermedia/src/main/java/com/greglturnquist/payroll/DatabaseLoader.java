43da5039aa89c817dc8d99564617b7f65d095674
Mariana F. Pereira <1231845@isep.ipp.pt>
2024-03-12 16:18:11 +0000

    [Feat] #5 #7 added email field validatin and created new branch

--- CA1/hypermedia/src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
+++ CA1/hypermedia/src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
@@ -23,25 +23,19 @@ import org.springframework.stereotype.Component;
  * @author Greg Turnquist
  */
 // tag::code[]
-@Component
-public class DatabaseLoader implements CommandLineRunner {
+@Component // <1>
+public class DatabaseLoader implements CommandLineRunner { // <2>
 
 	private final EmployeeRepository repository;
 
-	@Autowired
+	@Autowired // <3>
 	public DatabaseLoader(EmployeeRepository repository) {
 		this.repository = repository;
 	}
 
 	@Override
-	public void run(String... strings) throws Exception {
-
-		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
-		this.repository.save(new Employee("Bilbo", "Baggins", "burglar"));
-		this.repository.save(new Employee("Gandalf", "the Grey", "wizard"));
-		this.repository.save(new Employee("Samwise", "Gamgee", "gardener"));
-		this.repository.save(new Employee("Meriadoc", "Brandybuck", "pony rider"));
-		this.repository.save(new Employee("Peregrin", "Took", "pipe smoker"));
+	public void run(String... strings) throws Exception { // <4>
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer", 3, "burglar", "bilbobaggins@lordoftherings.com"));
 	}
 }
 // end::code[]
